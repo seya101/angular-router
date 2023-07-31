c26236eff976f2ec4ebaea86e1d35e8946f0965e
seya101 <hoseacorpuz@gmail.com>
2023-07-31 14:14:16 +0800

    add pageNotFound and use redirection w/ wildcardRoutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const AppRoutes: Routes = [
   { path:'', component: HomeComponent },
@@ -28,6 +29,8 @@ const AppRoutes: Routes = [
     ]
   },
 
+  { path: 'not-found', component: PageNotFoundComponent},
+  { path: '**', redirectTo: '/not-found'}
   
 ]
 
@@ -39,7 +42,8 @@ const AppRoutes: Routes = [
     ServersComponent,
     UserComponent,
     EditServerComponent,
-    ServerComponent
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
