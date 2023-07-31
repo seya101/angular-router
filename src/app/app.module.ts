d505a491cf6c77880b81d5276a74de6847b14a9e
seya101 <hoseacorpuz@gmail.com>
2023-07-31 14:32:31 +0800

    outsource the routeConfig

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
@@ -11,28 +12,10 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
-import { RouterModule, Routes } from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AppRoutingModule } from './app-routing.module';
 
-const AppRoutes: Routes = [
-  { path:'', component: HomeComponent },
-  { path:'users', component: UsersComponent,
-    children: [
-      { path:':id/:name', component: UserComponent },
-    ]
-  },
 
-  { path:'servers', component: ServersComponent, 
-    children: [
-      { path:':id', component: ServerComponent },
-      { path:':id/edit', component: EditServerComponent },
-    ]
-  },
-
-  { path: 'not-found', component: PageNotFoundComponent},
-  { path: '**', redirectTo: '/not-found'}
-  
-]
 
 @NgModule({
   declarations: [
@@ -48,7 +31,7 @@ const AppRoutes: Routes = [
   imports: [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot(AppRoutes)
+    AppRoutingModule
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
